3328e1a412a96e2852a6d17f6fce957f3c4d91da
Sakthi Sureshbabu <sakthisureshbabu26@gmail.com>
2025-03-23 10:09:17 +0530

    Styling alterations are carried out

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ export default function App() {
 
   return(
     <div className='App'>
-      <button onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
       <video width="250" ref={ref} onPlay={() => setIsPlaying(true)} onPause={() => setIsPlaying(false)}>
         <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type='video/mp4' />
       </video>
+      <button className='btn' onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
     </div>
   );
 }
